364a2ca8793101444e5023fe7bd10ed17aae771b
TaigahG <114576796+TaigahG@users.noreply.github.com>
2022-12-16 19:09:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Add your answer here
 $(".lesson").hover(
   function () {
-    $(this).find(".text-contents").show();
+    $(this).find(".text-contents").fadeIn(500);
   },
   function () {
-    $(this).find(".text-contents").hide();
+    $(this).find(".text-contents").fadeOut(500);
   }
 );
